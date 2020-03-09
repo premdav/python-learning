4c00d2f097be9e6e64197e3cfd911c3b5f07bbfe
Preston Davis <prem.dav@outlook.com>
2020-03-09 11:06:48 -0500

    finishes simple django course

--- python-learning/mysite/main/views.py
+++ python-learning/mysite/main/views.py
@@ -19,7 +19,10 @@ def single_slug(request, single_slug):
 
   tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
   if single_slug in tutorials:
-    return HttpResponse(f'{single_slug} is a tutorial')
+    this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+    tuts_from_series = Tutorial.objects.filter(tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by('tutorial_published')
+    this_tutorial_idx = list(tuts_from_series).index(this_tutorial)
+    return render(request, 'main/tutorial.html', { 'tutorial': this_tutorial, 'sidebar': tuts_from_series, 'this_tutorial_idx': this_tutorial_idx })
 
   return HttpResponse(f'{single_slug} does not correspond to any categories')
 
